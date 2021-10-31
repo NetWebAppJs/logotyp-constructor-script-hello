63cc8792bb0074278398cd0e61efd4209842d1f2
Andrew Berlin <78539122+berlinandrew@users.noreply.github.com>
2021-10-31 23:06:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
-consoleText(['ConstructorScript -', 'проект Берлина Андрея', 'Добро пожаловать!'], 'text', ['tomato', 'rebeccapurple', 'lightblue']);
+consoleText(['ConstructorScript -', 'проект Берлина Андрея', 'Добро пожаловать!'], 'text', ['#47ffbf', 'rebeccapurple', 'lightblue']);
 
 function consoleText(words, id, colors) {
     if (colors === undefined) colors = ['#fff'];
